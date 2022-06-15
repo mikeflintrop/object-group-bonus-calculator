61b49464c0564cf30ef9bc1ff94e19161fd13503
Mike Flintrop <mike.flintrop@gmail.com>
2022-06-14 19:20:10 -0500

    started two functions and have started to break down into smaller problems

--- client.js
+++ client.js
@@ -39,4 +39,39 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+// let name = employees.name;
+// let bonusPercentage = calculateEmployeeBonus(employees[i])
+// let totalBonus = employees[i].annualSalary * bonusPercentage
+// let totalCompensation = totalBonus + employees[i].annualSalary
+
+function showEmployeeBonuses(employee) {
+  console.log( 'in showEmployeeBonuses' );
+  const employeeInfo = {};
+  for (let i = 0; i < employee.length; i++){
+    if (employee[i].name) {
+    employeeInfo.push(employee[i].name, employeeInfo[i].bonusPercentage, 
+      employeeInfo[i].totalBonus, employeeInfo[i].totalCompensation);
+    }    
+  }
+  return employeeInfo;
+}
+
+function calculateEmployeeBonus(employees){
+  console.log( 'in calculateRemainingBudget' );
+  let bonusPercentage = 0;
+    if (employees.reviewRating === 1) 
+    bonusPercentage = 0;
+    else if(employees.reviewRating === 2)
+    bonusPercentage = 0;
+    else if(employees.reviewRating === 3)
+    bonusPercentage = 0.04;
+    else if(employees.reviewRating === 4)
+    bonusPercentage = 0.06;
+    else if(employees.reviewRating === 5)
+    bonusPercentage = 0.1;
+  console.log('Total Bonus:', bonusPercentage);
+  return bonusPercentage
+}
+
+console.log(calculateEmployeeBonus(employees[0]));
+console.log( employees )
